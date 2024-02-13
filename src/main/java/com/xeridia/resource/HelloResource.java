7ca4eb7c79d1c54f325e5b99c7788fcff223acc0
Sergio Bajo <sergio.bajo@xeridia.com>
2024-02-13 16:37:00 +0100

    first commit

---
+++ src/main/java/com/xeridia/resource/HelloResource.java
@@ -0,0 +1,29 @@
+package com.xeridia.resource;
+
+import com.xeridia.service.HelloService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @Inject
+    HelloService helloService;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/polite/{name}")
+    public String hello(@PathParam("name") String name) {
+        return helloService.politeHello(name);
+    }
+}
---
+++ src/main/java/com/xeridia/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.xeridia.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class HelloService {
+
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
+    public String politeHello(String name){
+        return greeting + " " + name;
+    }
+}
